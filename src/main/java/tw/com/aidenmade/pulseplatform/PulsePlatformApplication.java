dd958f70e0f495aac855e2213255b9c77c9495ca
AidenChen <wei310129@gmail.com>
2026-04-17 15:37:43 +0800

    init project

---
+++ src/main/java/tw/com/aidenmade/pulseplatform/PulsePlatformApplication.java
@@ -0,0 +1,13 @@
+package tw.com.aidenmade.pulseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulsePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/com/aidenmade/pulseplatform/PulsePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.aidenmade.pulseplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
